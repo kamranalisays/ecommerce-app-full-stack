9160d3f2a7bb57cfd8f1ad63690fddc3e3738230
MASTER\allfo <kamranali.says@gmail.com>
2024-10-15 10:55:22 +0100

    Adding bootstrap  responsive behavior code navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import Layout from './components/Layout/Layout';
+import React from "react";
 
+import { Routes, Route } from "react-router-dom";
 
-const  App = () =>  {
+import Layout from "./components/Layout/Layout";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
+
+const App = () => {
   return (
-    <> 
-    <Layout>  <h1> Layout </h1> </Layout>
-   </>
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/policy" element={<Policy />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Layout>
+    </>
   );
-}
+};
 
 export default App;
