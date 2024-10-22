69e26c412c8394f2f34069700f4866c2b3b5bfa7
MASTER\allfo <kamranali.says@gmail.com>
2024-10-22 02:08:23 +0100

    Setting up Page Not Found.

--- frontend/src/pages/PageNotFound.js
+++ frontend/src/pages/PageNotFound.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import './PageNotFound.css';
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <div className="pgnf">
-        <h1> 404 </h1>
-        <p> Page Not Found </p>
+        <h1 className="pgnf-title"> 404 </h1>
+        <h2 className="pgnf-heading"> Page Not Found </h2>
+        <Link to="/" className="pgnf-btn"> Go Back </Link>
     </div>
   );
 };
