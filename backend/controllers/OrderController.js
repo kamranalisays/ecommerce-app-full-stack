850a46817e7f5199ac25e6039c5f553a85ee92ea
MASTER\allfo <kamranali.says@gmail.com>
2024-11-21 06:03:29 +0000

    Adding getallOrders functionality

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -1,24 +1,17 @@
 import Order from "../models/Order.js";
 
 const submitOrder = async (req, res) => {
-  const order = new Order({
-    totalCost: 500,
-    orderDate: new Date(),
-  });
-
-  await order.save();
-
   return res.status(200).send({
     sucess: true,
   });
 };
 
 const getAllOrders = async (req, res) => {
-  const order = new Order({});
+  const order = await Order.find({});
 
   return res.status(200).send({
     sucess: true,
-    orders,
+    order,
   });
 };
 
--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -3,6 +3,8 @@ import OrderController from "../controllers/OrderController.js";
 
 const orderRoutes = express.Router();
 
+orderRoutes.get("/list", OrderController.getAllOrders);
+
 orderRoutes.post("/submitorder", OrderController.submitOrder);
 
 export default orderRoutes;
